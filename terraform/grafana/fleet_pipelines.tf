f58893d590e851f79ff31275a87e774620de0d4c
Rasmus "Pez" Wejlgaard <pez@pez.sh>
2026-05-08 19:46:00 +0100

    Update fleet_pipelines.tf

--- terraform/grafana/fleet_pipelines.tf
+++ terraform/grafana/fleet_pipelines.tf
@@ -25,3 +25,28 @@ resource "grafana_fleet_management_pipeline" "this" {
   matchers = each.value.matchers
   contents = file("${path.module}/fleet_pipelines/${each.key}.alloy")
 }
+
+import {
+  to = module.grafana.grafana_fleet_management_pipeline.this["linux_node_linux"]
+  id = "linux_node_linux"
+}
+
+import {
+  to = module.grafana.grafana_fleet_management_pipeline.this["octopus_exporter"]
+  id = "octopus_exporter"
+}
+
+import {
+  to = module.grafana.grafana_fleet_management_pipeline.this["plex"]
+  id = "plex"
+}
+
+import {
+  to = module.grafana.grafana_fleet_management_pipeline.this["caddy_linux"]
+  id = "caddy_linux"
+}
+
+import {
+  to = module.grafana.grafana_fleet_management_pipeline.this["docker_linux"]
+  id = "docker_linux"
+}
