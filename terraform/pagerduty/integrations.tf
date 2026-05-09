d3e94f15e7dd327b73216a6bf14d7db59f0f1fa6
Rasmus Wejlgaard <pez@pez.sh>
2026-05-09 20:48:32 +0100

    remove vendor

--- terraform/pagerduty/integrations.tf
+++ terraform/pagerduty/integrations.tf
@@ -1,11 +1,7 @@
-data "pagerduty_vendor" "prometheus" {
-  name = "Prometheus"
-}
-
 resource "pagerduty_service_integration" "grafana_cloud" {
   name    = "Grafana"
   service = pagerduty_service.pez_solutions.id
-  vendor  = data.pagerduty_vendor.prometheus.id
+  vendor  = ""
 }
 
 output "pagerduty_integration_key" {
