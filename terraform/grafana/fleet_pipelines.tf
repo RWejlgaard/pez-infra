8aa1afd2b8ca41511b5393b2af1d4735c1f96c90
Rasmus Wejlgaard <pez@pez.sh>
2026-05-10 10:16:54 +0100

    fix: tracing on caddy services

--- terraform/grafana/fleet_pipelines.tf
+++ terraform/grafana/fleet_pipelines.tf
@@ -1,5 +1,8 @@
 locals {
   fleet_pipelines = {
+    caddy_tracing = {
+      matchers = ["collector.ID=\"helsinki-a\""]
+    }
     linux_node_linux = {
       matchers = ["collector.os=\"linux\""]
     }
@@ -25,3 +28,4 @@ resource "grafana_fleet_management_pipeline" "this" {
   matchers = each.value.matchers
   contents = file("${path.module}/fleet_pipelines/${each.key}.alloy")
 }
+
